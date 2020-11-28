afa7f044003e9fc16b488466be0af732f34c3f34
aimoip <mohsen4812asgari@gmail.com>
2020-11-29 00:58:55 +0330

    Update EnvMultipleStock_train.py

--- env/EnvMultipleStock_train.py
+++ env/EnvMultipleStock_train.py
@@ -17,27 +17,29 @@ INITIAL_ACCOUNT_BALANCE=1000000
 STOCK_DIM = 1
 # transaction fee: 1/1000 reasonable percentage
 TRANSACTION_FEE_PERCENT = 0.001
+
+# turbulence index: 90-150 reasonable threshold
+#TURBULENCE_THRESHOLD = 140
 REWARD_SCALING = 1e-4
 
-class StockEnvTrain(gym.Env):
+class StockEnvValidation(gym.Env):
     """A stock trading environment for OpenAI gym"""
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, df,day = 0):
+    def __init__(self, df, day = 0, turbulence_threshold=140, iteration=''):
         #super(StockEnv, self).__init__()
         #money = 10 , scope = 1
         self.day = day
         self.df = df
-
         # action_space normalization and shape is STOCK_DIM
         self.action_space = spaces.Box(low = -1, high = 1,shape = (STOCK_DIM,)) 
         # Shape = 181: [Current Balance]+[prices 1-30]+[owned shares 1-30] 
         # +[macd 1-30]+ [rsi 1-30] + [cci 1-30] + [adx 1-30]
         self.observation_space = spaces.Box(low=0, high=np.inf, shape = (7,))
         # load data from a pandas dataframe
-        print(self.df)
         self.data = self.df.loc[self.day,:]
-        self.terminal = False             
+        self.terminal = False     
+        self.turbulence_threshold = turbulence_threshold
         # initalize state
         self.state = [INITIAL_ACCOUNT_BALANCE] + \
                       [self.data.close] + \
@@ -48,45 +50,64 @@ class StockEnvTrain(gym.Env):
                       [self.data.adx]
         # initialize reward
         self.reward = 0
+        self.turbulence = 0
         self.cost = 0
+        self.trades = 0
         # memorize all the total balance change
         self.asset_memory = [INITIAL_ACCOUNT_BALANCE]
         self.rewards_memory = []
-        self.trades = 0
         #self.reset()
         self._seed()
+        
+        self.iteration=iteration
 
 
     def _sell_stock(self, index, action):
         # perform sell action based on the sign of the action
-        if self.state[index+STOCK_DIM+1] > 0:
-            #update balance
-            self.state[0] += \
-            self.state[index+1]*min(abs(action),self.state[index+STOCK_DIM+1]) * \
-             (1- TRANSACTION_FEE_PERCENT)
-
-            self.state[index+STOCK_DIM+1] -= min(abs(action), self.state[index+STOCK_DIM+1])
-            self.cost +=self.state[index+1]*min(abs(action),self.state[index+STOCK_DIM+1]) * \
-             TRANSACTION_FEE_PERCENT
-            self.trades+=1
+        if self.turbulence<self.turbulence_threshold:
+            if self.state[index+STOCK_DIM+1] > 0:
+                #update balance
+                self.state[0] += \
+                self.state[index+1]*min(abs(action),self.state[index+STOCK_DIM+1]) * \
+                 (1- TRANSACTION_FEE_PERCENT)
+                
+                self.state[index+STOCK_DIM+1] -= min(abs(action), self.state[index+STOCK_DIM+1])
+                self.cost +=self.state[index+1]*min(abs(action),self.state[index+STOCK_DIM+1]) * \
+                 TRANSACTION_FEE_PERCENT
+                self.trades+=1
+            else:
+                pass
         else:
-            pass
-
+            # if turbulence goes over threshold, just clear out all positions 
+            if self.state[index+STOCK_DIM+1] > 0:
+                #update balance
+                self.state[0] += self.state[index+1]*self.state[index+STOCK_DIM+1]* \
+                              (1- TRANSACTION_FEE_PERCENT)
+                self.state[index+STOCK_DIM+1] =0
+                self.cost += self.state[index+1]*self.state[index+STOCK_DIM+1]* \
+                              TRANSACTION_FEE_PERCENT
+                self.trades+=1
+            else:
+                pass
     
     def _buy_stock(self, index, action):
         # perform buy action based on the sign of the action
-        available_amount = self.state[0] // self.state[index+1]
-        # print('available_amount:{}'.format(available_amount))
+        if self.turbulence< self.turbulence_threshold:
+            available_amount = self.state[0] // self.state[index+1]
+            # print('available_amount:{}'.format(available_amount))
+            
+            #update balance
+            self.state[0] -= self.state[index+1]*min(available_amount, action)* \
+                              (1+ TRANSACTION_FEE_PERCENT)
 
-        #update balance
-        self.state[0] -= self.state[index+1]*min(available_amount, action)* \
-                          (1+ TRANSACTION_FEE_PERCENT)
-
-        self.state[index+STOCK_DIM+1] += min(available_amount, action)
-
-        self.cost+=self.state[index+1]*min(available_amount, action)* \
-                          TRANSACTION_FEE_PERCENT
-        self.trades+=1
+            self.state[index+STOCK_DIM+1] += min(available_amount, action)
+            
+            self.cost+=self.state[index+1]*min(available_amount, action)* \
+                              TRANSACTION_FEE_PERCENT
+            self.trades+=1
+        else:
+            # if turbulence goes over threshold, just stop buying
+            pass
         
     def step(self, actions):
         # print(self.day)
@@ -95,25 +116,28 @@ class StockEnvTrain(gym.Env):
 
         if self.terminal:
             plt.plot(self.asset_memory,'r')
-            plt.savefig('results/account_value_train.png')
+            plt.savefig('results/account_value_validation_{}.png'.format(self.iteration))
             plt.close()
+            df_total_value = pd.DataFrame(self.asset_memory)
+            print("at the step and trying to write account_value_validation in iteration #",self.iteration)
+            df_total_value.to_csv('results/account_value_validation_{}.csv'.format(self.iteration))
             end_total_asset = self.state[0]+ \
             sum(np.array(self.state[1:(STOCK_DIM+1)])*np.array(self.state[(STOCK_DIM+1):(STOCK_DIM*2+1)]))
-            
+            #print("previous_total_asset:{}".format(self.asset_memory[0]))           
+
             #print("end_total_asset:{}".format(end_total_asset))
-            df_total_value = pd.DataFrame(self.asset_memory)
-            df_total_value.to_csv('results/account_value_train.csv')
-            #print("total_reward:{}".format(self.state[0]+sum(np.array(self.state[1:(STOCK_DIM+1)])*np.array(self.state[(STOCK_DIM+1):61]))- INITIAL_ACCOUNT_BALANCE ))
+            #print("total_reward:{}".format(self.state[0]+sum(np.array(self.state[1:(STOCK_DIM+1)])*np.array(self.state[(STOCK_DIM+1):61]))- self.asset_memory[0] ))
             #print("total_cost: ", self.cost)
-            #print("total_trades: ", self.trades)
+            #print("total trades: ", self.trades)
+
             df_total_value.columns = ['account_value']
             df_total_value['daily_return']=df_total_value.pct_change(1)
-            sharpe = (252**0.5)*df_total_value['daily_return'].mean()/ \
+            sharpe = (4**0.5)*df_total_value['daily_return'].mean()/ \
                   (df_total_value['daily_return'].std()+0.000000001)
             #print("Sharpe: ",sharpe)
-            #print("=================================")
-            df_rewards = pd.DataFrame(self.rewards_memory)
-            #df_rewards.to_csv('results/account_rewards_train.csv')
+            
+            #df_rewards = pd.DataFrame(self.rewards_memory)
+            #df_rewards.to_csv('results/account_rewards_trade_{}.csv'.format(self.iteration))
             
             # print('total asset: {}'.format(self.state[0]+ sum(np.array(self.state[1:29])*np.array(self.state[29:]))))
             #with open('obs.pkl', 'wb') as f:  
@@ -126,7 +150,8 @@ class StockEnvTrain(gym.Env):
 
             actions = actions * HMAX_NORMALIZE
             #actions = (actions.astype(int))
-            
+            if self.turbulence>=self.turbulence_threshold:
+                actions=np.array([-HMAX_NORMALIZE]*STOCK_DIM)
             begin_total_asset = self.state[0]+ \
             sum(np.array(self.state[1:(STOCK_DIM+1)])*np.array(self.state[(STOCK_DIM+1):(STOCK_DIM*2+1)]))
             #print("begin_total_asset:{}".format(begin_total_asset))
@@ -146,6 +171,9 @@ class StockEnvTrain(gym.Env):
 
             self.day += 1
             self.data = self.df.loc[self.day,:]         
+            print("all self.data['turbulence']",self.data['turbulence'])
+            self.turbulence = self.data['turbulence']
+            #print(self.turbulence)
             #load next state
             # print("stock_shares:{}".format(self.state[29:]))
             self.state =  [self.state[0]] + \
@@ -155,7 +183,8 @@ class StockEnvTrain(gym.Env):
                       [self.data.rsi] + \
                       [self.data.cci] + \
                       [self.data.adx]
-            print("self state in train env is ",self.state)
+            print("self state in val env is ",self.state)
+
             end_total_asset = self.state[0]+ \
             sum(np.array(self.state[1:(STOCK_DIM+1)])*np.array(self.state[(STOCK_DIM+1):(STOCK_DIM*2+1)]))
             self.asset_memory.append(end_total_asset)
@@ -167,17 +196,17 @@ class StockEnvTrain(gym.Env):
             
             self.reward = self.reward*REWARD_SCALING
 
-
-
         return self.state, self.reward, self.terminal, {}
 
-    def reset(self):
+    def reset(self):  
         self.asset_memory = [INITIAL_ACCOUNT_BALANCE]
         self.day = 0
         self.data = self.df.loc[self.day,:]
+        self.turbulence = 0
         self.cost = 0
         self.trades = 0
         self.terminal = False 
+        #self.iteration=self.iteration
         self.rewards_memory = []
         #initiate state
         self.state = [INITIAL_ACCOUNT_BALANCE] + \
@@ -187,11 +216,12 @@ class StockEnvTrain(gym.Env):
                       [self.data.rsi] + \
                       [self.data.cci] + \
                       [self.data.adx]
-        # iteration += 1 
+
         return self.state
     
-    def render(self, mode='human'):
+    def render(self, mode='human',close=False):
         return self.state
+    
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
